ef0b545291a788d21faa0f7c1a5449ad8881f32c
Brian Lacey <ss3teddy@gmail.com>
2026-07-03 00:06:40 -0700

    Updated regex slightly to accomodate period for punctuation.

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -2,7 +2,7 @@ export const statusList = ["Open", "In Progress", "Resolved"];
 
 export const priorityList = ["Low", "Medium", "High"];
 
-export const textRegex = /^[a-zA-Z0-9 -]+$/;
+export const textRegex = /^[a-zA-Z0-9 .-]+$/;
 
 export const emailRegex =
   /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?(?:\.[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?)*$/i;
